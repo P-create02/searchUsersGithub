1a48db99183db5182120b8b5f3e96798d17cea6d
Wojciech Sikora <74451896+P-create02@users.noreply.github.com>
2021-05-10 14:14:55 +0200

    Update PrivateRoute.js

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,13 +3,13 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 
-const PrivateRoute = ({children,...rest}) => { // rest to to w Route -> "exact path='/'"
+const PrivateRoute = ({children,...rest}) => { 
   const { user, isAuthenticated } = useAuth0()
-  // const isUser = false // to jest jakby nasz 'PrivateRoute' z App.js = true or false
+  
   const isUser = isAuthenticated && user
 
   return <Route {...rest} render={() =>{
-    return isUser ? children: <Redirect to='/login' /> // Redirect -> take back user to..
+    return isUser ? children: <Redirect to='/login' />
   }}></Route>
 };
 
